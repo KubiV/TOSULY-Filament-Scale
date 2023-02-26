9d4f69febf287fc509c620887b6bf71fbc3b84f5
Jakub Vávra <56688914+KubiV@users.noreply.github.com>
2023-02-26 22:13:24 +0100

    Update fw0.1.ino

--- Software/fw0.1/fw0.1.ino
+++ Software/fw0.1/fw0.1.ino
@@ -4,7 +4,8 @@
 #include <math.h>
 
 #define CalibrationFactor 375 // Calibarate your LOAD CELL with 100g weight, and change the value according to readings
-#define SpoolMass         250
+#define SpoolMass         250 // Insert mass of your empty filament spool in grams
+
 float j = 0;
 
 PinButton myButton(2);
@@ -13,7 +14,7 @@ HX711_ADC LoadCell(4, 5); // dt pin, sck pin
 U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);
 String drawing = "0";
 
-void drawMass(String input) 
+void drawMass(String input) //draws mass readings on OLED
 {
    u8g.setFont(u8g_font_fub17r);   // select font
    u8g.setPrintPos(0, 20);
@@ -27,7 +28,7 @@ void setup() {
   
   LoadCell.begin(); // start connection to HX711
   LoadCell.start(2000); // load cells gets 1000ms of time to stabilize
-  LoadCell.setCalFactor(CalibrationFactor); 
+  LoadCell.setCalFactor(CalibrationFactor); // calibration of load cell
 
   Serial.begin(9600);
 
@@ -37,9 +38,9 @@ void loop() {
 
   myButton.update();
   LoadCell.update(); // retrieves data from the load cell
-  float i = LoadCell.getData(); // get output value
+  float i = LoadCell.getData(); // get output value from the load cell
 
-  if (myButton.isDoubleClick()) { //subtract defined spool mass
+  if (myButton.isDoubleClick()) { //subtracts defined spool mass
     Serial.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
     j = -SpoolMass;
   } 
@@ -47,22 +48,21 @@ void loop() {
   Serial.print(round(i+j));
   Serial.println(" g");
 
-  if (i>=5000){
+  if (i>=5000){ // overload warning
     i=0;
     Serial.print("Over Loaded"); 
     delay(200);
   }
   
-  if (myButton.isSingleClick()) {
+  if (myButton.isSingleClick()) { // taring information
     Serial.println("Taring");
     j = 0;
     LoadCell.start(2000);
   }
 
-
   u8g.firstPage();  
    do {
    drawMass(String(round(i+j)));
    } while( u8g.nextPage() );
-      //delay(500);
+   //delay(500);
 }
